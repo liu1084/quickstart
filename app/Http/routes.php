24b2afe92849574bd7f281802ac37d3e33d6bab3
jim <liu1084@gmail.com>
2015-12-31 02:40:52 +0800

    laravel project init

---
+++ app/Http/routes.php
@@ -0,0 +1,44 @@
+<?php
+use App\Task;
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+
+    Route::get('/', function(){
+        return view('task/tasks');
+    });
+
+    Route::post('/task', function(Request $request){
+
+    });
+
+    Route::delete('/task/{task}', function (Task $task) {
+        //
+    });
+});
