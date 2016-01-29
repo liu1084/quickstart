153ba24c96591296857881e4189bd1a61fa7ad3e
liujun <liu.jun@nhn.com>
2016-01-29 14:20:06 +0800

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,5 @@
 <?php
 use App\Task;
-use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Routes File
@@ -28,39 +27,6 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
-
-//    Route::get('/', function(){
-//        $tasks = Task::orderBy('created_at', 'asc')->get();
-//        return view('task/tasks', [
-//            'tasks' => $tasks
-//        ]);
-//    });
-
-//    Route::post('/task', function(Request $request){
-//        $validator = Validator::make($request->all(), [
-//            'name' => 'required|max:255'
-//        ]);
-//
-//        if ($validator->fails()){
-//            return redirect('/')
-//                ->withInput()
-//                ->withErrors($validator);
-//        }
-//
-//        $task = new Task();
-//        $task->name = $request->name;
-//        $task->save();
-//
-//        return redirect('/');
-//    });
-//
-//    Route::delete('/task/{task}', function (Task $task) {
-//        //
-//
-//        $task->delete();
-//        return redirect('/');
-//    });
 
     // Authentication Routes...
     Route::auth();
